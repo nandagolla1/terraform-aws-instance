ec647c4da4410ffc6191c618c4660667b79fd7d5
nanda <nandagolla074@gmail.com>
2025-07-03 10:26:50 +0530

    terraform module

---
+++ variables.tf
@@ -0,0 +1,22 @@
+variable "ami_id" {
+  type = string
+  default = "ami-09c813fb71547fc4f"
+}
+
+variable "instance_type" {
+  type = string
+  default = "t3.micro"
+  
+  validation {
+    condition     = contains(["t3.micro", "t3.small", "t3.medium"], var.instance_type)
+    error_message = "Valid values for instance_type: t3.micro, t3.small, t3.medium"
+  }
+}
+
+variable "security_group_ids" {
+  type = list(string)
+}
+
+variable "tags" {
+  type = map()
+}
